627d827d4ab8b3a8de94f3298c80d16fc7ee5e53
Sümeyye Kılıçoğlu <smyyeklcogl@icloud.com>
2022-09-16 21:50:02 +0300

    Initial Commit

---
+++ CustomLabel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomLabel
+//
+//  Created by Sümeyye Kılıçoğlu on 16.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
